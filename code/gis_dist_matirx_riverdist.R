d28c56bb2dc4a71b70f41091b5f566c591797bca
cfwahl <89475052+cfwahl@users.noreply.github.com>
2022-02-03 13:18:40 -0500

    added data for all watersheds, distance matrices, and R code
    added site and stream network shapefiles for all seven watersheds, added instream network distances for all seven watersheds, and added R code to create distance matrix using riverdist function

---
+++ code/gis_dist_matirx_riverdist.R
@@ -0,0 +1,69 @@
+
+# setup -------------------------------------------------------------------
+
+pacman::p_load(riverdist,
+              dplyr,
+              sf)
+
+# set working directory 
+
+# data --------------------------------------------------------------------
+
+# read sites
+utm_sf_outlet <- st_read(dsn = "data_raw/watersheds/watershed1",
+                      layer = "epsg3722_sites_ws1",
+                      drivers = "ESRI Shapefile")
+
+# create dataframe 
+df <- utm_sf_outlet %>% 
+  mutate(X = st_coordinates(.)[,1],
+         Y = st_coordinates(.)[,2]) %>% 
+  as_tibble()
+
+# coordinates  from utm_sf_outlet 
+X <- df$X
+Y <- df$Y
+
+
+# read in the network
+strnet <- line2network(path="data_raw/watersheds/watershed1", 
+                       layer="epsg3722_StrNet_ws1")
+
+
+# prep stream netowrk and sites --------------------------------------------------
+
+# plot of all the segments
+plot(strnet)  
+
+# check topology (nodes)
+topologydots(strnet)
+
+# do a clean up: dissolve - y, insert vertices - y, distance - 1, 
+#     examine figure for mouth questions, remove additional segments - n, 
+#     build segment routes - y
+strnet_fixed <- cleanup(rivers = strnet)
+
+# snap sites to stream network
+site_snap <- xy2segvert(x=X, y=Y, rivers=strnet_fixed)
+
+# add vertices
+site_strnet <- bind_cols(utm_sf_outlet, site_snap)
+
+# map
+plot(x=strnet_fixed, color = FALSE)
+points(X, Y, pch=16, col="red") # raw coords
+riverpoints(seg=site_snap$seg, vert=site_snap$vert, rivers=strnet_fixed, 
+            pch=16, cex=0.5, bg="forestgreen") # snapped coords
+text(X, Y, labels=site_strnet$segment, pos = 4)
+
+
+# distance matrix  --------------------------------------------------------
+
+# create matrix of distances
+dmat <- riverdistancemat(site_strnet$seg, site_strnet$vert, strnet_fixed, 
+                         ID = site_strnet$segment ) %>% as.matrix
+dmat <- dmat/1000 # convert to km
+
+# write the data out
+site_dists <- as.data.frame(dmat)
+write.csv(site_dists,"data_raw/watersheds/watershed1\\siteXdist_ws1.csv", row.names = TRUE)
